e6ecab1743e0362bb35b10250c5573d1472822c4
Vineet101024 <vineetcba2025@email.iimcal.ac.in>
2026-09-19 01:13:32 +0530

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,22 @@
+# Restaurant menu data structure
+MENU_DATA = {
+    "Starters": {
+        "Garlic Bread": 5.99,
+        "Bruschetta": 6.50,
+        "Chicken Wings": 8.99,
+        "Paneer Tikka": 7.99
+    },
+    "Main Course": {
+        "Margherita Pizza": 12.99,
+        "Pepperoni Pizza": 14.99,
+        "Creamy Alfredo Pasta": 13.50,
+        "Grilled Chicken Steak": 16.00,
+        "Veggie Burger": 10.99
+    },
+    "Beverages": {
+        "Coca Cola": 2.50,
+        "Iced Lemon Tea": 3.00,
+        "Fresh Lime Soda": 2.80,
+        "Chocolate Milkshake": 4.50
+    }
+}
